09ac11abf492dd3d02b47d04e4bb33644c19f28e
Dan Barber <dbarber@gmail.com>
2023-11-25 06:51:04 -0500

    Updated with CRUD operation logic

--- src/s3Handler.js
+++ src/s3Handler.js
@@ -4,12 +4,53 @@ const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'us-west-2'  // Set your AWS region
 });
 
 const s3 = new AWS.S3();
+const bucketName = process.env.S3_BUCKET_NAME;
 
-// Functions for create, read, update, delete operations
+// Create - Upload a file to S3
+const uploadFile = async (fileName, fileContent) => {
+    const params = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileContent
+    };
+    return s3.upload(params).promise();
+};
+
+// Read - Download a file from S3
+const getFile = async (fileName) => {
+    const params = {
+        Bucket: bucketName,
+        Key: fileName
+    };
+    return s3.getObject(params).promise();
+};
+
+// Update - Update an existing file in S3
+const updateFile = async (fileName, fileContent) => {
+    const params = {
+        Bucket: bucketName,
+        Key: fileName,
+        Body: fileContent
+    };
+    return s3.upload(params).promise();
+};
+
+// Delete - Delete a file from S3
+const deleteFile = async (fileName) => {
+    const params = {
+        Bucket: bucketName,
+        Key: fileName
+    };
+    return s3.deleteObject(params).promise();
+};
 
 module.exports = {
-  // export your functions here
+  uploadFile,
+  getFile,
+  updateFile,
+  deleteFile
 };
--- tests/s3Handler.test.js
+++ tests/s3Handler.test.js
@@ -1,10 +1,31 @@
 
 const s3Handler = require('../src/s3Handler');
+const AWS = require('aws-sdk-mock');
 
 describe('S3 Handler Tests', () => {
-  test('Create File', async () => {
-    // Mock AWS SDK calls and test create functionality
+  beforeEach(() => {
+    AWS.mock('S3', 'upload', Promise.resolve({}));
+    AWS.mock('S3', 'getObject', Promise.resolve({}));
+    AWS.mock('S3', 'deleteObject', Promise.resolve({}));
   });
 
-  // Add tests for read, update, delete
+  afterEach(() => {
+    AWS.restore('S3');
+  });
+
+  test('Upload File', async () => {
+    await expect(s3Handler.uploadFile('test.txt', 'content')).resolves.toEqual({});
+  });
+
+  test('Get File', async () => {
+    await expect(s3Handler.getFile('test.txt')).resolves.toEqual({});
+  });
+
+  test('Update File', async () => {
+    await expect(s3Handler.updateFile('test.txt', 'new content')).resolves.toEqual({});
+  });
+
+  test('Delete File', async () => {
+    await expect(s3Handler.deleteFile('test.txt')).resolves.toEqual({});
+  });
 });
